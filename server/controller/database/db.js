1a70ccca297ea2e1d960e9543ab9bc185d1e3f50
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2024-05-06 12:34:16 +0530

    Create db.js

---
+++ server/controller/database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@whatsapp-clone.fqers4o.mongodb.net/`;
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
